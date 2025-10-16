0dcd8af7131eed120e550e5114d3c11d870ec88c
Toa <toateachestech@gmail.com>
2025-10-16 16:00:10 -0600

    Initial Project Setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// Write you functions here:
+// 1) Each function should accept points as an input
+// 2) Each function should display a prompt quizzing the user on the correct answer to a piece of USU trivia (You choose the trivia question)
+// 3) If they answer correctly, return the input points + however many points you feel they deserve for being correct. 
+// 4) Otherwise, return the original score
+// 5) Name your function according to the guidelines on canvas (e.g. round1, round2, etc.)
+// If you need more guidence, feel free to check index.html to see the main flow of the program.
