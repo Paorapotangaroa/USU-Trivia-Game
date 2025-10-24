9181ce5b4a0dce6ba423c9cf819213a6fe1fcb6f
Faith Newbold <a02360201@usu.edu>
2025-10-23 19:06:00 -0600

    Create round 3
    Created round 3 of the USU trivia game.

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
 // 4) Otherwise, return the original score
 // 5) Name your function according to the guidelines on canvas (e.g. round1, round2, etc.)
 // If you need more guidence, feel free to check index.html to see the main flow of the program.
+
+// Round 3
+
+function round3(points) {
+    let answer = parseFloat(prompt("How many stairs are there on old main?"));
+    if (answer === 124) {
+        alert("Wow you know your aggie trivia!")
+        return points + 20;
+    } else {
+        alert("Wrong answer. Guess you gotta take more walks up old main hill.")
+        return points;
+    };
+};
+
+export {round3};
